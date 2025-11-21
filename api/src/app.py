a816fbb148836a3ad8532ec9ffe07833c553ee09
aidandawley <aidandawley62@gmail.com>
2025-11-21 01:02:35 -0800

    Completed Get endpoint

--- api/src/app.py
+++ api/src/app.py
@@ -8,6 +8,8 @@ from typing_extensions import TypedDict
 
 from services.database import JSONDatabase
 
+from datetime import datetime, timedelta
+from fastapi import Query
 
 class Quote(TypedDict):
     name: str
@@ -48,3 +50,39 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
 
 
 # TODO: add another API route with a query parameter to retrieve quotes based on max age
+
+#helper function to compute time thresholds
+def get_time_delta(max_age: str) -> timedelta | None:
+    """
+    Convert age string into timedelta.
+    """
+    if max_age == "week":
+        return timedelta(days=7)
+    if max_age == "month":
+        return timedelta(days=30)
+    if max_age == "year":
+        return timedelta(days=365)
+    return None
+
+
+@app.get("/quotes")
+def get_quotes(max_age: str = Query("all")) -> list[Quote]:
+    quotes = database["quotes"]
+    """
+    Retrieve quotes from the data base by filter.
+    Time filters incliude: week, month, year
+    """
+    delta = get_time_delta(max_age)
+    if delta is None:
+        return quotes  
+
+    cutoff = datetime.now() - delta
+
+    filtered = []
+    for q in quotes:
+        quote_time = datetime.fromisoformat(q["time"])
+        if quote_time >= cutoff:
+            filtered.append(q)
+ 
+ 
+    return filtered
